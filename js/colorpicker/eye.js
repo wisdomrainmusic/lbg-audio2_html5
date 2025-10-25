99ed732c9ebad83bb0450b727bb93c6d2c57f957
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-10-26 01:34:41 +0300

    Add files via upload

---
+++ js/colorpicker/eye.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * Zoomimage
+ * Author: Stefan Petre www.eyecon.ro
+ *
+ */
+(function(d){var e=window.EYE=function(){var c={init:[]};return{init:function(){d.each(c.init,function(b,a){a.call()})},extend:function(b){for(var a in b)void 0!=b[a]&&(this[a]=b[a])},register:function(b,a){c[a]||(c[a]=[]);c[a].push(b)}}}();d(e.init)})(jQuery);
